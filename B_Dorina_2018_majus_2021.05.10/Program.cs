9bea26d9a2ca9d72c5be153fe57eefc9a8ce922a
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-05-10 13:41:32 +0200

    Add project files.

---
+++ B_Dorina_2018_majus_2021.05.10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Dorina_2018_majus_2021._05._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
